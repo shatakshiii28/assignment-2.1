c63d0abe31b3888bc0f6129ca778330427b45031
shatakshiii28 <sawantshatakshi28@gmail.com>
2026-03-23 23:56:09 +0530

    Add files via upload

---
+++ asssigment2_q3.py
@@ -0,0 +1,15 @@
+# Type casting example
+
+a = 255        # integer
+b = 2.55      # float
+c = "Python"      # string
+
+x = float(a)   # int to float
+y = int(b)     # float to int
+z = int(c)     # string to int
+
+print(x)
+print(y)
+print(z)
+
+
