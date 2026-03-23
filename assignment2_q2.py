8715f9a461d70e3bc493e84e0f96b32e5087b56c
shatakshiii28 <sawantshatakshi28@gmail.com>
2026-03-23 23:55:40 +0530

    Add files via upload

---
+++ assignment2_q2.py
@@ -0,0 +1,11 @@
+# Declaring variables
+a = 255          # integer
+b = 3.14          # float
+c = "Shatakshi"       # string
+d = False          # boolean
+
+print("Value of a:", a, "Type:", type(a))
+print("Value of b:", b, "Type:", type(b))
+print("Value of c:", c, "Type:", type(c))
+print("Value of d:", d, "Type:", type(d))
+
